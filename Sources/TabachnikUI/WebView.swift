357e529724c2297f54efe78d06b9b9dd75714432
Ron Tabachnik <rontabachnik@gmail.com>
2025-01-17 20:52:09 +0200

    Remove recent changes

--- Sources/TabachnikUI/WebView.swift
+++ Sources/TabachnikUI/WebView.swift
@@ -12,53 +12,23 @@ import UIKit
 
 public struct WebViewWrapper: UIViewRepresentable {
     public let urlString: String
-    @Binding public var originalUrl: URL?
 
     public func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        context.coordinator.originalUrl = URL(string: urlString) // Save the original URL
-        webView.navigationDelegate = context.coordinator
-        return webView
+        return WKWebView()
     }
 
     public func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let url = URL(string: urlString), url != context.coordinator.currentUrl {
-            let request = URLRequest(url: url)
-            uiView.load(request)
-        }
-    }
-
-    public func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    public class Coordinator: NSObject, WKNavigationDelegate {
-        var parent: WebViewWrapper
-        var originalUrl: URL?
-        var currentUrl: URL?
-
-        init(_ parent: WebViewWrapper) {
-            self.parent = parent
-        }
-
-        public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            currentUrl = webView.url
-        }
-
-        func navigateToOriginalUrl(in webView: WKWebView) {
-            if let originalUrl = originalUrl {
-                let request = URLRequest(url: originalUrl)
-                webView.load(request)
-            }
-        }
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
 
+// WebView struct with a custom navigation bar and embedded WebViewWrapper
 @available(iOS 15.0, *)
 public struct WebView<Content: View>: View {
     public let url: String
     public let header: () -> Content?
-    @State private var originalUrl: URL?
 
     public init(url: String, @ViewBuilder header: @escaping () -> Content? = { EmptyView() }) {
         self.url = url
@@ -67,22 +37,8 @@ public struct WebView<Content: View>: View {
     
     public var body: some View {
         VStack {
-            HStack {
-                if let originalUrl = originalUrl {
-                    Button(action: {
-                        NotificationCenter.default.post(
-                            name: Notification.Name("NavigateToOriginalUrl"),
-                            object: originalUrl
-                        )
-                    }) {
-                        Text("Go to Original URL")
-                    }
-                }
-                Spacer()
-            }
-            .padding()
-
-            WebViewWrapper(urlString: url, originalUrl: $originalUrl)
+            header()
+            WebViewWrapper(urlString: url)
                 .navigationBarHidden(true)
         }
     }
